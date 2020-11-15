97a3e03ec76bfee741f5b968781f6997d28bd5ee
Kamil Frankiewicz <kamil.frankiewicz@satlan.pl>
2020-11-15 23:31:30 +0100

    dotfiles definitons folder :hammer:

--- reload.sh
+++ reload.sh
@@ -1,14 +1,15 @@
 #!/bin/sh 
 
-filez=(".vimrc")
-
-for file in ${filez[@]}; do
+config_files_path="$HOME/dotfiles/files"
+file_names=$(ls -A $config_files_path)
+ 
+for file in ${file_names[@]}; do
   if [ -f "$HOME/$file" ]; then
       echo "$HOME/$file exists, removing..."
       rm "$HOME/$file"
-  # else 
-  #     echo "$HOME/$file does not exist."
+  else 
+      echo "$HOME/$file does not exist."
   fi
-  echo "link $HOME/$file"
-  ln -nfs $HOME/dotfiles/$file $HOME/$file
+  echo "link $config_files_path/$file -> $HOME/$file"
+  ln -nfs $config_files_path/$file $HOME/$file
 done
