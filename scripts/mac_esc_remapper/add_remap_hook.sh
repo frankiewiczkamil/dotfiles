208900ec8e2014a5b028ff8bea704d6b4b95869e
kamil <frankiewiczkamil@gmail.com>
2023-05-07 11:56:30 +0200

    switch from deprecated login hook to launch agent :wrench: :arrow_up:

--- scripts/mac_esc_remapper/add_remap_hook.sh
+++ scripts/mac_esc_remapper/add_remap_hook.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
-sudo defaults write com.apple.loginwindow LoginHook ~/dotfiles/scripts/mac_esc_remapper/remap.sh
-
+sudo cp ~/dotfiles/scripts/mac_esc_remapper/remap.sh /Library/LaunchDaemons/remap.sh
+sudo cp ~/dotfiles/scripts/mac_esc_remapper/remap.plist /Library/LaunchDaemons/remap.plist
+sudo chown root:wheel /Library/LaunchDaemons/remap.sh
+sudo chmod 755 /Library/LaunchDaemons/remap.sh
+sudo chown root:wheel /Library/LaunchDaemons/remap.plist
+sudo chmod 644 /Library/LaunchDaemons/remap.plist
+sudo launchctl load /Library/LaunchDaemons/remap.plist
